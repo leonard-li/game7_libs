ba079d8a05c184c428d56bc519e6d30070080c98
李强 <liqiang@game7.cc>
2017-06-06 18:18:07 +0800

    update heyzap sdk to 9.10.2

--- HeyzapAds.framework/Versions/A/Headers/HZBannerAdOptions.h
+++ HeyzapAds.framework/Versions/A/Headers/HZBannerAdOptions.h
@@ -62,25 +62,79 @@ typedef NS_ENUM(NSUInteger, HZAdMobBannerSize){
     HZAdMobBannerSizeFullBanner,
 };
 
+/** The constant for a banner 320 points wide and 50 points high. */
+extern const CGSize HZInMobiBannerSize320x50;
+/** The constant for a banner 468 points wide and 60 points high. */
+extern const CGSize HZInMobiBannerSize468x60;
+/** The constant for a banner 480 points wide and 75 points high. */
+extern const CGSize HZInMobiBannerSize480x75;
+/** The constant for a banner 728 points wide and 90 points high. */
+extern const CGSize HZInMobiBannerSize728x90;
+
+/** The constant for a banner 50 pts in height whose width expands to fill its presentingViewController (default). */
+extern const CGSize HZHeyzapExchangeBannerSizeFlexibleWidthHeight50;
+/** The constant for a banner 32 pts in height whose width expands to fill its presentingViewController. */
+extern const CGSize HZHeyzapExchangeBannerSizeFlexibleWidthHeight32;
+/** The constant for a banner 90 pts in height whose width expands to fill its presentingViewController. */
+extern const CGSize HZHeyzapExchangeBannerSizeFlexibleWidthHeight90;
+/** The constant for a banner 100 pts in height whose width expands to fill its presentingViewController. */
+extern const CGSize HZHeyzapExchangeBannerSizeFlexibleWidthHeight100;
+/** The constant for a banner 320 points wide and 50 points high. */
+extern const CGSize HZHeyzapExchangeBannerSize320x50;
+/** The constant for a banner 468 points wide and 60 points high. */
+extern const CGSize HZHeyzapExchangeBannerSize468x60;
+/** The constant for a banner 480 points wide and 75 points high. */
+extern const CGSize HZHeyzapExchangeBannerSize480x75;
+/** The constant for a banner 728 points wide and 90 points high. */
+extern const CGSize HZHeyzapExchangeBannerSize728x90;
+
+/** The 320x50 size. This is the default for phones. */
+extern CGSize const HZMoPubBannerSize;
+/** The 728x90 size. This is the default for tablets. */
+extern CGSize const HZMoPubLeaderboardSize;
+
 @interface HZBannerAdOptions : NSObject <NSCopying>
 
 /**
  *  The size to use for Facebook Audience Network banners. Defaults to HZFacebookBannerSizeFlexibleWidthHeight50.
  */
 @property (nonatomic) HZFacebookBannerSize facebookBannerSize;
+
 /**
  *  The size to use for Admob banners.
  */
 @property (nonatomic) HZAdMobBannerSize admobBannerSize;
 
-// iAds does not offer sizing options. Please refer to the `ADBannerView` documentation for information on ad sizes.
+/**
+ *  The size to use for Heyzap Exchange banners.
+ *  Currently, any size is allowed to be requested, but depending on the exchange providers' inventory, an ad may be returned that is scaled or letterboxed to meet your request. Some constants (named `HZHeyzapExchangeBannerSize...`) are provided above for convenience. 
+ *  If you would like to provide your own `CGSize`, you may. Using `-1` for the width will indicate to the SDK that the banner should fill the width of the `presentingViewController`.
+ */
+@property (nonatomic) CGSize heyzapExchangeBannerSize;
+
+/**
+ *  InMobi allows setting an arbitrary size to use for banners, but only some intrinsic banner sizes are supported. 
+ *  It's recommended that you choose one of the `HZInMobiBannerSize` constants above.
+ *  If you choose an unsupported size, InMobi may scale a similar sized ad to the size you requested.
+ *
+ *  If you don't select a size, a default size is used based on the current orientation and interface idiom:
+ *  Portrait iPhones: `HZInMobiBannerSize320x50`
+ *  Landscape iPhones: `HZInMobiBannerSize468x60`
+ *  iPads: `HZInMobiBannerSize728x90`
+ */
+@property (nonatomic) CGSize inMobiBannerSize;
+
+@property (nonatomic) CGSize moPubBannerSize;
+
+/// @name Other Banner Options
+#pragma mark - Other Banner Options
 
 /**
  *  The view controller to present the ad from. 
  *
  *  This property is optional. If not set, it defaults to the root view controller of the application.
  *
- *  @note Setting this property doesn't change where the actual banner (a `UIView`) is placed.
+ *  @note Setting this property doesn't change where the actual banner (a `UIView`) is placed. For Heyzap Exchange banners, the width of this property will determine the width of a flexible-width banner.
  */
 @property (nonatomic, weak) UIViewController *presentingViewController;
 
--- HeyzapAds.framework/Versions/A/Headers/HeyzapAds.h
+++ HeyzapAds.framework/Versions/A/Headers/HeyzapAds.h
@@ -43,15 +43,21 @@
 #import "HZNativeAd.h"
 #import "HZNativeAdImage.h"
 
+#import "HZMediatedNativeAd.h"
+#import "HZMediatedNativeAdManager.h"
+#import "HZMediatedNativeAdViewRegisterer.h"
+
+#import "HZFetchOptions.h"
 #import "HZShowOptions.h"
 #import "HZBannerAd.h"
 #import "HZBannerAdOptions.h"
+#import "HZDemographics.h"
 
 #ifndef NS_ENUM
 #define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
 #endif
 
-#define SDK_VERSION @"9.2.8"
+#define SDK_VERSION @"9.10.2"
 
 #if __has_feature(objc_modules)
 @import AdSupport;
@@ -60,12 +66,11 @@
 @import MediaPlayer;
 @import QuartzCore;
 @import StoreKit;
-@import iAd;
 @import MobileCoreServices;
 @import Security;
 @import SystemConfiguration;
-@import EventKit;
-@import EventKitUI;
+@import MessageUI;
+@import CoreLocation;
 #endif
 
 typedef NS_ENUM(NSUInteger, HZAdOptions) {
@@ -90,9 +95,19 @@ typedef NS_ENUM(NSUInteger, HZAdOptions) {
      */
     HZAdOptionsDisableMedation = 1 << 3, // 8
     /**
-     * Pass this to disable recording of In-App Purchase data
+     *  Pass this to disable recording of In-App Purchase data
      */
     HZAdOptionsDisableAutomaticIAPRecording = 1 << 4, // 16
+    
+    //placeholder for android flag value NATIVE_ADS_ONLY = 1 << 5 // 32
+    // (iOS does not (yet) use this option, but iOS and Android need to keep the same flag values for the sake of Unity, AIR, etc.)
+    
+    /**
+     *  Pass this flag to mark mediated ads as "child-directed". This value will be passed on to networks that support sending such an option (for purposes of the Children's Online Privacy Protection Act (COPPA)).
+     *  This value will be `OR`ed with our server-side (per mediated network) setting, so setting it on the server, here, or both will enable it.
+     *  Currently, only AdMob, Facebook Audience Network, and Heyzap Exchange are passed this information.
+     */
+    HZAdOptionsChildDirectedAds = 1 << 6, // 64
 };
 
 
@@ -106,10 +121,10 @@ extern NSString * const HZNetworkVungle;
 extern NSString * const HZNetworkChartboost;
 extern NSString * const HZNetworkAdColony;
 extern NSString * const HZNetworkAdMob;
-extern NSString * const HZNetworkIAd;
 extern NSString * const HZNetworkHyprMX;
 extern NSString * const HZNetworkHeyzapExchange;
 extern NSString * const HZNetworkLeadbolt;
+extern NSString * const HZNetworkInMobi;
 
 // General Network Callbacks
 extern NSString * const HZNetworkCallbackInitialized;
@@ -146,6 +161,36 @@ extern NSString * const HZNetworkCallbackFacebookLoggingImpression;
 
 // NSNotifications
 extern NSString * const HZRemoteDataRefreshedNotification;
+extern NSString * const HZMediationNetworkCallbackNotification;
+// HZAdsDelegate Callback NSNotifications
+extern NSString * const HZMediationDidShowAdNotification;
+extern NSString * const HZMediationDidFailToShowAdNotification;
+extern NSString * const HZMediationDidReceiveAdNotification;
+extern NSString * const HZMediationDidFailToReceiveAdNotification;
+extern NSString * const HZMediationDidClickAdNotification;
+extern NSString * const HZMediationDidHideAdNotification;
+extern NSString * const HZMediationWillStartAdAudioNotification;
+extern NSString * const HZMediationDidFinishAdAudioNotification;
+// HZIncentivizedAdDelegate Callback NSNotifications
+extern NSString * const HZMediationDidCompleteIncentivizedAdNotification;
+extern NSString * const HZMediationDidFailToCompleteIncentivizedAdNotification;
+
+// User Info Keys for the HZMediationNetworkCallbackNotification
+/**
+ *  The corresponding value is the name of the network callback being sent (see above constants for the possible values).
+ */
+extern NSString * const HZNetworkCallbackNameUserInfoKey;
+
+// User Info Keys for HZAdsDelegate and HZIncentivizedAdDelegate NSNotifications
+/**
+ *  The corresponding value is the ad tag of the ad a NSNotification is being sent about.
+ */
+extern NSString * const HZAdTagUserInfoKey;
+/**
+ *  The corresponding value is the name of the network providing the ad a NSNotification is being sent about, if applicable.
+ */
+extern NSString * const HZNetworkNameUserInfoKey;
+
 
 /** The `HZAdsDelegate` protocol provides global information about our ads. If you want to know if we had an ad to show after calling `showAd` (for example, to fallback to another ads provider). It is recommend using the `showAd:completion:` method instead. */
 @protocol HZAdsDelegate<NSObject>
@@ -289,10 +334,22 @@ extern NSString * const HZRemoteDataRefreshedNotification;
 
 /**
  * Returns a dictionary of developer-settable data or an empty dictionary if no data is available.
- 
+ *
  * @note This data is cached, so it will usually be available at app launch. It is updated via a network call that is made when `[HeyzapAds startWithPublisherId:]` (or one of its related methods) is called. If you want to guarantee that the data has been refreshed, only use it after receiving an NSNotification with name=`HZRemoteDataRefreshedNotification`. The userInfo passed with the notification will be the same NSDictionary you can receive with this method call.
  */
 + (NSDictionary *) remoteData;
+
+/**
+ * Returns a string representation of the remote data dictionary. @see remoteData
+ */
++ (NSString *) getRemoteDataJsonString;
+
+/**
+ *  Returns an `HZDemographics` object that you can use to pass demographic information to third party SDKs.
+ *
+ *  @return An `HZDemographics` object. Guaranteed to be non-nil after starting the SDK.
+ */
++ (HZDemographics *)demographicInformation;
 
 /**
  * Presents a view controller that displays integration information and allows fetch/show testing
@@ -321,12 +378,6 @@ extern NSString * const HZRemoteDataRefreshedNotification;
  *  @see pauseExpensiveWork
  */
 + (void)resumeExpensiveWork;
-
-/**
- * Returns a raw json string of developer-settable data or an empty json string if no data is available.
- * 
- */
-+ (NSString *) getRemoteDataJsonString;
 
 
 #pragma mark - Record IAP Transaction
